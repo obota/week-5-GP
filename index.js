b201fec5c629758681c398e75de7301a9bac94fd
Dennis Obota <dobota@safaricom.co.ke>
2023-02-20 11:52:23 +0300

    This commit sets up online MongoDB server and connects it to the application

--- index.js
+++ index.js
@@ -14,16 +14,3 @@
 // DB.on('error', (error) => {
 //     console.log(error);
 // });
-
-// mongodb+srv://obota:BNXxQWmLkxu4RLh4@moringacluster.bulbv0v.mongodb.net/test
-
-//connect mongo db
-var MongoClient = require('mongodb').MongoClient;
-//Create a database named "mydb":
-var url = "mongodb+srv://obota:BNXxQWmLkxu4RLh4@moringacluster.bulbv0v.mongodb.net/test";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
