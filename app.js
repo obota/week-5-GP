f427f7a4d2be3fcfb5fe8f980bf1f36d13250c2d
Dennis Obota <dobota@safaricom.co.ke>
2023-02-20 11:51:00 +0300

    This commit sets up online MongoDB server and connects it to the application

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-//entry point of node js
-var http = require('http'); 
+var http = require("http");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Your node server is running on port 7000');
-}).listen(7000);
+//CONNECT TO MONGO DB
+mongoose.connect(
+  "mongodb+srv://obota:BNXxQWmLkxu4RLh4@moringacluster.bulbv0v.mongodb.net/test",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfuly connected to MongoDB");
+    }
+  }
+);
 
+//CONNECT TO NODE JS
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write(req.url);
+    res.end(
+      "Your node server is running on port 7,000. We are cruising nicely"
+    );
+  })
+  .listen(7000);
+//display connection on terminal
 console.log("Server running on port 7,000");
--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const databaseConnectionString = process.env.MONGO_CONNECTION_STRING
+// const databaseConnectionString = process.env.MONGO_CONNECTION_STRING
 
-mongoose.connect(databaseConnectionString);
-const DB = mongoose.connection;
+// mongoose.connect(databaseConnectionString);
+// const DB = mongoose.connection;
 
-DB.once('connected', () => {
-    console.log('Database connected successfully');
-});
+// DB.once('connected', () => {
+//     console.log('Database connected successfully');
+// });
 
-DB.on('error', (error) => {
-    console.log(error);
+// DB.on('error', (error) => {
+//     console.log(error);
+// });
+
+// mongodb+srv://obota:BNXxQWmLkxu4RLh4@moringacluster.bulbv0v.mongodb.net/test
+
+//connect mongo db
+var MongoClient = require('mongodb').MongoClient;
+//Create a database named "mydb":
+var url = "mongodb+srv://obota:BNXxQWmLkxu4RLh4@moringacluster.bulbv0v.mongodb.net/test";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
 });
